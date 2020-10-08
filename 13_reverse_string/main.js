8dfd03e277644bbc00e9ece137b0ff1d65f89384
vertefra <verte.fra@gmail.com>
2020-10-08 13:44:59 -0400

    solved with reducer

--- 13_reverse_string/main.js
+++ 13_reverse_string/main.js
@@ -1,5 +1,9 @@
+// const reverseString = (string) => {
+//   return string.split("").reverse().join("");
+// };
+
 const reverseString = (string) => {
-  return string.split("").reverse().join("");
+  return string.split("").reduce((rev, char) => char + rev, "");
 };
 
 module.exports = reverseString;
