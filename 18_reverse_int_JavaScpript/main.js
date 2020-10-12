cd17524533f30e818db4cbc0566bbd3252ae9b2d
vertefra <verte.fra@gmail.com>
2020-10-12 10:25:54 -0400

    negative test cases added

--- 18_reverse_int_JavaScpript/main.js
+++ 18_reverse_int_JavaScpript/main.js
@@ -4,7 +4,8 @@
 // reverseInt(542) === 245
 
 const reverseInt = (n) => {
-    return parseInt(n.toString().split('').reverse().join(''))
+    const sign = Math.sign(n) === -1 ? '-' : '+'
+    return parseInt(sign+n.toString().split('').reverse().join(''))
 }
 
 module.exports = reverseInt
--- 18_reverse_int_JavaScpript/test.spec.js
+++ 18_reverse_int_JavaScpript/test.spec.js
@@ -1,4 +1,5 @@
 const { expect } = require('@jest/globals')
+const { reverse } = require('dns')
 const reverseInt = require('./main.js')
 
 describe("testing reverseInt function", ()=>{
@@ -8,5 +9,13 @@ describe("testing reverseInt function", ()=>{
 
     test("reverseInt(100098) to be 890001", ()=>{
         expect(reverseInt(100098)).toBe(890001)
+    })
+
+    test("reverseInt(-90) to be -9", ()=>{
+        expect(reverseInt(-90)).toBe(-9)
+    })
+
+    test("everseInt(-51) to be -15", ()=>{
+        expect(reverseInt(-51)).toBe(-15)
     })
 })
