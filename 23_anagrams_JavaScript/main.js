1537138491639cb3a1d6c330bfc77cd5bc77100e
vertefra <verte.fra@gmail.com>
2020-10-16 07:52:45 -0400

    second solution

--- 23_anagrams_JavaScript/main.js
+++ 23_anagrams_JavaScript/main.js
@@ -4,27 +4,32 @@
 // Only consider characters, not punctuation.
 // Consider capital letters to be the same as lowercase
 
+// const anagram = (strA, strB) => {
+//   const hashStrA = mapString(strA);
+//   const hashStrB = mapString(strB);
+
+//   if (Object.keys(hashStrA).length !== Object.keys(hashStrB).length)
+//     return false;
+
+//   for (let char in hashStrA) {
+//     if (hashStrB[char] !== hashStrA[char]) return false;
+//   }
+
+//   return true;
+// };
+
+// const mapString = str => {
+//   const obj = {};
+//   for (let char of str.replace(/^\w*$/g, '').toLowerCase()) {
+//     obj[char] = obj[char] + 1 || 1;
+//   }
+//   return obj;
+// };
+
 const anagram = (strA, strB) => {
-  const hashStrA = mapString(strA);
-  const hashStrB = mapString(strB);
-
-  if (Object.keys(hashStrA).length !== Object.keys(hashStrB).length)
-    return false;
-
-  for (let char in hashStrA) {
-    if (hashStrB[char] !== hashStrA[char]) return false;
-  }
-
-  return true;
-};
-
-const mapString = str => {
-  const obj = {};
-  for (let char of str) {
-    char = char.toLowerCase();
-    if (char.match(/^\w*$/g)) obj[char] = obj[char] + 1 || 1;
-  }
-  return obj;
+  strA = strA.replace(/^w*$/g, '').toLowerCase().split('').sort().join('');
+  strB = strB.replace(/^w*$/g, '').toLowerCase().split('').sort().join('');
+  return strA === strB;
 };
 
 module.exports = anagram;
