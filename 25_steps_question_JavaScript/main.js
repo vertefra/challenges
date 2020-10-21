ffc54f4f46685bda64bc3a615a19737bc30361e9
vertefra <verte.fra@gmail.com>
2020-10-21 09:22:28 -0400

    recursive 1

--- 25_steps_question_JavaScript/main.js
+++ 25_steps_question_JavaScript/main.js
@@ -1,20 +1,37 @@
-const steps = n => {
-  const line = initLine(n);
-
-  for (let i = 0; i < n; i++) {
-    line[i] = '#';
-    const str = line.join('');
-    console.log(str);
+const steps = (n, idx = 0) => {
+  if (idx === n) {
+    return undefined;
+  } else {
+    let buf = '';
+    for (i = 0; i < n; i++) {
+      if (i <= idx) {
+        buf += '#';
+      } else {
+        buf += ' ';
+      }
+    }
+    console.log(buf);
   }
+  steps(n, idx + 1);
 };
 
-const initLine = n => {
-  const backArr = [];
-  for (let i = 1; i <= n; i++) {
-    backArr.push(' ');
-  }
+// const steps = n => {
+//   const line = initLine(n);
 
-  return backArr;
-};
+//   for (let i = 0; i < n; i++) {
+//     line[i] = '#';
+//     const str = line.join('');
+//     console.log(str);
+//   }
+// };
+
+// const initLine = n => {
+//   const backArr = [];
+//   for (let i = 1; i <= n; i++) {
+//     backArr.push(' ');
+//   }
+
+//   return backArr;
+// };
 
 module.exports = steps;
