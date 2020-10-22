c13bd789767ee4d673bfe57c2f60042f28cc6b09
vertefra <verte.fra@gmail.com>
2020-10-22 08:35:12 -0400

    my resursive solution

--- 26_twosided_steps_JavaScript/main.js
+++ 26_twosided_steps_JavaScript/main.js
@@ -1,35 +1,61 @@
-const pyramid = n => {
-  const line = initLine(n);
+const pyramid = (
+  n,
+  step = 0,
+  line = '',
+  midpoint = Math.floor((n * 2 - 1) / 2)
+) => {
+  if (step === n) {
+    return;
+  }
 
-  const medianIndex = Math.floor(line.length / 2);
-  let inc = 0;
-
-  for (let step = 1; step <= n; step++) {
-    for (let idx = 0; idx < line.length; idx++) {
-      if (idx >= medianIndex - inc && idx <= medianIndex + inc) {
-        line[idx] = '#';
-      }
+  if (line.length < n * 2 - 1) {
+    if (line.length >= midpoint - step && line.length <= midpoint + step) {
+      line += '#';
+    } else {
+      line += ' ';
     }
-    console.log(line.join(''));
-    inc++;
+    return pyramid(n, step, line, midpoint);
   }
+
+  if (line.length === n * 2 - 1) {
+    console.log(line);
+  }
+
+  pyramid(n, step + 1, '', midpoint);
 };
 
-const initLine = n => {
-  const line = [];
+// const pyramid = n => {
+//   const line = initLine(n);
 
-  let width;
+//   const medianIndex = Math.floor(line.length / 2);
+//   let inc = 0;
 
-  if (n === 1) {
-    width = 1;
-  } else {
-    width = n * 2 - 1;
-  }
+//   for (let step = 1; step <= n; step++) {
+//     for (let idx = 0; idx < line.length; idx++) {
+//       if (idx >= medianIndex - inc && idx <= medianIndex + inc) {
+//         line[idx] = '#';
+//       }
+//     }
+//     console.log(line.join(''));
+//     inc++;
+//   }
+// };
 
-  for (let i = 1; i <= width; i++) {
-    line.push(' ');
-  }
-  return line;
-};
+// const initLine = n => {
+//   const line = [];
+
+//   let width;
+
+//   if (n === 1) {
+//     width = 1;
+//   } else {
+//     width = n * 2 - 1;
+//   }
+
+//   for (let i = 1; i <= width; i++) {
+//     line.push(' ');
+//   }
+//   return line;
+// };
 
 module.exports = pyramid;
