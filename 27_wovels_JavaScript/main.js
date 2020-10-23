bda3002edabb17b88865b177c26f4bcdbe508637
vertefra <verte.fra@gmail.com>
2020-10-23 07:39:51 -0400

    regex solution

--- 27_wovels_JavaScript/main.js
+++ 27_wovels_JavaScript/main.js
@@ -1,8 +1,13 @@
 const wovels = str => {
-  return str.split('').reduce((acc, l) => {
-    return ['a', 'e', 'i', 'o', 'u'].includes(l.toLowerCase()) ? acc + 1 : acc;
-  }, 0);
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 };
+
+// const wovels = str => {
+//   return str.split('').reduce((acc, l) => {
+//     return ['a', 'e', 'i', 'o', 'u'].includes(l.toLowerCase()) ? acc + 1 : acc;
+//   }, 0);
+// };
 
 wovels('alambicco');
 
