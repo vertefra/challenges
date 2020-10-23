eb023b917fe5e162d763ce999d6d1d95e98a6a4b
vertefra <verte.fra@gmail.com>
2020-10-23 07:33:33 -0400

    wovels with reducer

---
+++ 27_wovels_JavaScript/main.js
@@ -0,0 +1,9 @@
+const wovels = str => {
+  return str.split('').reduce((acc, l) => {
+    return ['a', 'e', 'i', 'o', 'u'].includes(l.toLowerCase()) ? acc + 1 : acc;
+  }, 0);
+};
+
+wovels('alambicco');
+
+module.exports = wovels;
