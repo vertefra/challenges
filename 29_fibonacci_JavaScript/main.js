0a80f96ea3fa4374b26228afc56dada612ed0675
vertefra <verte.fra@gmail.com>
2020-10-31 13:04:14 -0400

    fibo recursion

--- 29_fibonacci_JavaScript/main.js
+++ 29_fibonacci_JavaScript/main.js
@@ -1,13 +1,21 @@
+// const fib = n => {
+//   const res = [0, 1]
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = res[i - 2]
+//     const b = res[i - 1]
+
+//     res.push(a + b)
+//   }
+
+//   return res[n]
+// }
+
 const fib = n => {
-  const res = [0, 1]
-
-  for (let i = 2; i <= n; i++) {
-    const a = res[i - 2]
-    const b = res[i - 1]
-
-    res.push(a + b)
+  if (n < 2) {
+    return n
   }
 
-  return res[n]
+  return fib(n - 1) + fib(n - 2)
 }
 module.exports = fib
