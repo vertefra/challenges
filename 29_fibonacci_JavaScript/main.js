fd7b8584847861f99d3738da407f2536540a6ad9
vertefra <verte.fra@gmail.com>
2020-11-09 09:55:01 -0500

    memoization

--- 29_fibonacci_JavaScript/main.js
+++ 29_fibonacci_JavaScript/main.js
@@ -11,11 +11,37 @@
 //   return res[n]
 // }
 
-const fib = n => {
+let fib = n => {
   if (n < 2) {
     return n
   }
-
+  // when you call a memoized function recursively make sure that it's calling the memoized version
+  // so the return statement should refer to line 23 memoized version of the function
   return fib(n - 1) + fib(n - 2)
 }
+
+fib = memoize(fib)
+
+
+// Fibonacci solution is vvery bad for time complexity
+// Memoization
+
+function memoize(fn){
+  const cache = {}
+
+  return (...args) => {
+    
+    if (cache[args]){
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args)
+    cache[args] = result
+
+    return result
+  }
+}
+
+
+
 module.exports = fib
