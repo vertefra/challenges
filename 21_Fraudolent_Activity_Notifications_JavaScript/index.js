c07a3e6049b4d10fd5cb0724775f680908e4ddb6
vertefra <verte.fra@gmail.com>
2020-12-07 08:49:22 -0500

    solved in O(n^2)

--- 21_Fraudolent_Activity_Notifications_JavaScript/index.js
+++ 21_Fraudolent_Activity_Notifications_JavaScript/index.js
@@ -1,35 +1,42 @@
 const d = 5; // days used to calculate the median
-const n = [2, 3, 4, 2, 3, 6, 8, 4, 5];
+const n = [10, 20, 30, 40, 50];
 
-const median = n => {
-  const numbers = [...n];
-  numbers.sort((a, b) => a - b);
-  if (numbers.length % 2 !== 0) {
-    return numbers[Math.floor(numbers.length / 2)];
-  } else {
-    const n = numbers.length / 2;
-    const n2 = n - 1;
-    return (numbers[n] + numbers[n2]) / 2;
-  }
+const median = (n) => {
+	const numbers = [...n];
+	numbers.sort((a, b) => a - b);
+	if (numbers.length % 2 !== 0) {
+		return numbers[Math.floor(numbers.length / 2)];
+	} else {
+		const n = numbers.length / 2;
+		const n2 = n - 1;
+		return (numbers[n] + numbers[n2]) / 2;
+	}
 };
 
+// console.log(median([3, 4, 7, 3, 2])); // => 2, 3, 3, 4, 7 => median 3
+// console.log(median([2, 4, 3, 5])); // 2, 3, 4, 5 => (3+4) /2 => median 3.5
+
+// d = trailing days to calculate the median
+// expenditures = array with integer repr. expenditure values
+
+// receive a notification if current expense is >= to 2 x the current expenditures median value
+
 function activityNotifications(expenditure, d) {
-  let notifications = 0;
-  let currDay = 1;
-  for (let exp of expenditure) {
-    if (!(currDay <= d)) {
-      const days = expenditure.slice(currDay - 1 - d, currDay - 1);
-      const m = median(days);
-      console.log('calc median on slice ', days);
-      console.log(`median for exp ${exp} is ${m}`);
-      exp >= 2 * m ? notifications++ : null;
-    }
-    currDay++;
-  }
-  return notifications;
+	let trailingExpenditures = expenditure.splice(0, d);
+
+	let notifications = 0;
+	for (let currentExp of expenditure) {
+		let currentMedian = median(trailingExpenditures);
+		if (currentExp >= currentMedian * 2) {
+			notifications++;
+		}
+		trailingExpenditures.shift();
+		trailingExpenditures.push(currentExp);
+	}
+
+	return notifications;
 }
 
-const b = activityNotifications(n, d);
-console.log(b);
+console.log(activityNotifications(n, 3));
 
-module.exports = activityNotifications;
+// module.exports = activityNotifications;
