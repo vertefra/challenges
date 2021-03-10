03611dca70fe5e4e08620dcc2ce396c6918950b4
Francesco Vertemati <fvertemati@nexvortex.com>
2021-03-10 10:03:36 -0500

    ssh test

--- 32_linked_list_javascript/LinkedList.js
+++ 32_linked_list_javascript/LinkedList.js
@@ -25,6 +25,48 @@ class LinkedList {
 
 		return count;
 	}
+
+	getFirst() {
+		return this.head.data;
+	}
+
+	getLast() {
+		let current_node = this.head;
+
+		while (current_node.next) {
+			current_node = current_node.next;
+		}
+
+		return current_node.data;
+	}
+
+	clear() {
+		this.head = null;
+	}
+
+	removeFirst() {
+		if (!this.head) {
+			return;
+		}
+		this.head = this.head.next;
+	}
+
+	removeLast() {
+		if (!this.head) {
+			return;
+		}
+		if (!this.head.next) {
+			this.head = null;
+			return;
+		}
+		let current_node = this.head;
+		let previous_node = null;
+		while (current_node.next) {
+			previous_node = current_node;
+			current_node = current_node.next;
+		}
+		previous_node.next = null;
+	}
 }
 
 const list = new LinkedList();
@@ -32,4 +74,11 @@ list.insertFirst(456);
 list.insertFirst(343);
 list.insertFirst(233);
 console.log(list);
-console.log(list.size());
+console.log('Size: ', list.size());
+console.log('Head: ', list.getFirst());
+console.log('Tail: ', list.getLast());
+list.removeFirst();
+console.log(list);
+list.removeLast();
+list.removeLast();
+console.log(list);
